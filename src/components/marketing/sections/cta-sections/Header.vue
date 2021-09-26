584f1d4f531173b6a967fa4777f063c8a4e069f4
ehab-hassan <hassane@codescalers.com>
2021-09-26 09:06:06 +0200

    update header for support page

--- src/components/marketing/sections/cta-sections/Header.vue
+++ src/components/marketing/sections/cta-sections/Header.vue
@@ -426,101 +426,63 @@
       </div>
       <g-image class="order-2 lg:order-none" :src="img" :alt="altImg" />
     </div>
-    <div class="flex flex-wrap text-center lg:text-left lg:pt-10 -mx-2" v-else>
-      <div
-        class="px-2 lg:py-40 lg:mt-10 order-1 lg:order-none mx-auto text-center"
-      >
-        <h2 class="text-8xl uppercase mb-6 mt-20 leading-none font-heading">
-          <span class="">{{ slogan }}</span
-          ><br />
-          {{ title }}
+    <div
+      class="flex flex-wrap text-center lg:text-left lg:mt-10 lg:pt-10 -mx-2"
+      v-else
+    >
+      <div class="lg:w-2/5 px-2 lg:pr-20 lg:mt-10 order-1 lg:order-none">
+        <h2 class="text-6xl mb-6 mt-20 leading-none font-heading">
+          <span class="text-blue-600">{{ slogan }}</span> {{ title }}
         </h2>
-        <div class="mb-8 text-lg leading-relaxed" v-html="excerpt"></div>
-
-        <a
-          v-if="link.includes('http')"
-          target="_blank"
-          class="
-            inline-block
-            bg-white
-            text-sm
-            learn-button
-            hover:bg-gray-400
-            bo
-            px-12
-            py-1
-            mr-5
-            mb-4
-            border-2
-            shadow
-            border-black
-          "
-          :href="link"
-          >{{ button }}</a
-        >
-        <a
-          v-else
-          class="
-            inline-block
-            bg-white
-            text-sm
-            learn-button
-            hover:bg-gray-400
-            bo
-            px-12
-            py-1
-            mr-5
-            mb-4
-            border-2
-            shadow
-            border-black
-          "
-          :href="link"
-          >{{ button }}</a
-        >
-        <a
-          class="
-            inline-block
-            bg-white
-            text-sm
-            learn-button
-            hover:bg-gray-400
-            bo
-            px-12
-            py-1
-            mr-5
-            mb-4
-            border-2
-            shadow
-            border-black
-          "
-          :href="link2"
-          >{{ button2 }}</a
-        >
-        <a
-          class="
-            inline-block
-            bg-white
-            text-sm
-            learn-button
-            hover:bg-gray-400
-            bo
-            px-12
-            py-1
-            mr-5
-            mb-4
-            border-2
-            shadow
-            border-black
-          "
-          :href="link3"
-          >{{ button3 }}</a
-        >
-        <!-- <a class="text-gray-600 hover:underline" href="#">Learn more</a> -->
+        <div class="mb-8 text-gray-700 leading-relaxed" v-html="excerpt"></div>
+        <div v-if="button">
+          <a
+            v-if="link.includes('http')"
+            target="_blank"
+            class="
+              inline-block
+              bg-blue-900
+              text-sm
+              learn-button
+              hover:bg-blue-800
+              text-gray-100
+              px-12
+              py-2
+              mr-5
+              mb-4
+              rounded
+              shadow
+              rounded-full
+            "
+            :href="link"
+            >{{ button }}</a
+          >
+          <a
+            v-else
+            class="
+              inline-block
+              bg-blue-900
+              text-sm
+              learn-button
+              hover:bg-blue-800
+              text-gray-100
+              px-12
+              py-2
+              mr-5
+              mb-4
+              rounded
+              shadow
+              rounded-full
+            "
+            :href="link"
+            >{{ button }}</a
+          >
+          <!-- <a class="text-gray-600 hover:underline" href="#">Learn more</a> -->
+        </div>
       </div>
-      <!-- <div class="lg:w-3/5 px-2 self-center">
+      <div class="lg:w-3/5 px-2 self-center">
         <g-image :src="img" :alt="altImg" />
-      </div> -->
+      </div>
     </div>
   </section>
 </template>
